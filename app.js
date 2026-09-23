dfc89ffcf55005956e5de781533fcb53109c3ae3
zunigaalaskatherinelissette-cloud <zunigaalaskatherinelissetteB@gmail.com>
2026-09-23 08:42:50 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById('btnCambio');
+const aviso = document.getElementById('aviso');
+btn.addEventListener('click', () => {
+ aviso.textContent = "Sistema de tránsito operativo";
+});
