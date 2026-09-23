59fb4fc967509da4128557d0f852c12cc008ce4e
donovanoliva093 <donovanoliva09@gmail.com>
2026-09-23 08:51:56 -0600

    feat: implementa cambio a luz verde

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const aviso = document.getElementById('aviso');
 btn.addEventListener('click', () => {
  aviso.textContent = "Sistema de tránsito operativo";
 });
+// Estudiante A activa paso verde
+document.getElementById('luz-roja').classList.remove('activo');
+document.getElementById('luz-verde').classList.add('activo');
+aviso.textContent = "ADELANTE - VÍA LIBRE";
